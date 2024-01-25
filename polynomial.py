5c7ec367df64388371119193ea6858139adfec59
PrathmeshS <prs.lowell@gmail.com>
2024-01-25 00:42:15 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -20,6 +20,24 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        if isinstance(self.p1, (Mul, Div)):
+            p1_repr = "( " + repr(self.p1) + " )"
+        else:
+            p1_repr = repr(self.p1)
+
+        if isinstance(self.p2, (Mul, Div)):
+            p2_repr = "( " + repr(self.p2) + " )"
+        else:
+            p2_repr = repr(self.p2)
+
+        return p1_repr + " - " + p2_repr
+
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -32,8 +50,30 @@ class Mul:
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+
+        if isinstance(self.p1, Sub):
+            if isinstance(self.p2, Sub):
+                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
+        if isinstance(self.p2, Sub):
+            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+
         return repr(self.p1) + " * " + repr(self.p2)
 
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        if isinstance(self.p1, (Add, Sub, Mul)):
+            p1_repr = "( " + repr(self.p1) + " )"
+        else:
+            p1_repr = repr(self.p1)
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+        if isinstance(self.p2, (Add, Sub, Mul)):
+            p2_repr = "( " + repr(self.p2) + " )"
+        else:
+            p2_repr = repr(self.p2)
+
+        return p1_repr + " / " + p2_repr
